5a180b6e9194cd0c495ec97c28671f327300225f
Vasiliy Yurkovets <vyurkovets@int.codinsula.com>
2019-09-06 20:08:42 +0300

    cant finish in time

--- src/Components/Content/Main/Calendar/Calendar.js
+++ src/Components/Content/Main/Calendar/Calendar.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Calendar.module.css';
+import CalendarDateMenu from './DateMenu/CalendarDateMenu';
+import Navigation from './Navigation/Navigation';
+import TopBar from './TopBar/TopBar';
+import Table from './Table/Table';
+import monthNames from './months';
+
+const date = new Date();
 
 const Calendar = () => {
-    return <div className={css.calendar}>Calendar View</div>;
+    const [month, setMonth] = useState(date.getMonth());
+    const [year, setYear] = useState(date.getFullYear());
+
+    useEffect(() => {
+        // setMonth();
+        // setYear();
+    }, []);
+
+    return (
+        <div className={css.calendar}>
+            <CalendarDateMenu />
+            <div className={css.title}>
+                <Navigation
+                    month={month}
+                    year={year}
+                    setMonth={setMonth}
+                    setYear={setYear}
+                    date={date}
+                />
+                <p
+                    className={css.title_name}
+                >{`${monthNames[month]} ${year}`}</p>
+            </div>
+            <div className={css.calendar_table}>
+                <TopBar />
+                <Table month={month} year={year} date={date} />
+            </div>
+        </div>
+    );
 };
 
 export default Calendar;
--- src/Img/index.js
+++ src/Img/index.js
@@ -1,10 +1,13 @@
 import Avatar from './Avatar.png';
 import Arrow from './Arrow.svg';
+import Add from './Add.svg';
+import Timer from './Timer.svg';
 import Notification from './Notification.svg';
 import Search from './Search.svg';
 import Logo from './Logo.svg';
 import Calendar from './Calendar.svg';
 import Chat from './Chat.svg';
+import Close from './Close.svg';
 import Customers from './Customers.svg';
 import Dashboard from './Dashboard.svg';
 import Help from './Help.svg';
@@ -17,15 +20,18 @@ import Settings from './Settings.svg';
 export {
     Avatar,
     Arrow,
+    Add,
     Search,
     Notification,
     Logo,
     Calendar,
     Chat,
+    Close,
     Customers,
     Dashboard,
     Help,
     Home,
+    Timer,
     Inbox,
     Invoices,
     Products,
