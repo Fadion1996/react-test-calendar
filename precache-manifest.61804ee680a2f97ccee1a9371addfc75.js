43e683d2a1d75751f966c83724b7f85ed6ef28af
Vasiliy Yurkovets <vyurkovets@int.codinsula.com>
2019-09-06 20:14:56 +0300

    Updates

---
+++ precache-manifest.61804ee680a2f97ccee1a9371addfc75.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e6af26ac4b0d0d555bec74b08c5699",
+    "url": "/react-test-calendar/index.html"
+  },
+  {
+    "revision": "96070b3601649665af60",
+    "url": "/react-test-calendar/static/css/main.3d70dcaf.chunk.css"
+  },
+  {
+    "revision": "2697f28366c396d69415",
+    "url": "/react-test-calendar/static/js/2.763bcf27.chunk.js"
+  },
+  {
+    "revision": "96070b3601649665af60",
+    "url": "/react-test-calendar/static/js/main.e6839b17.chunk.js"
+  },
+  {
+    "revision": "49620d4edfb6a1c6602d",
+    "url": "/react-test-calendar/static/js/runtime~main.d872c602.js"
+  },
+  {
+    "revision": "ee21fc4a4f794834bd41b3638672ac7a",
+    "url": "/react-test-calendar/static/media/Add.ee21fc4a.svg"
+  },
+  {
+    "revision": "e1ad04b9073b58c59d433cefe09d161b",
+    "url": "/react-test-calendar/static/media/Arrow.e1ad04b9.svg"
+  },
+  {
+    "revision": "84b85e8d80d2675c02f9058416639985",
+    "url": "/react-test-calendar/static/media/Avatar.84b85e8d.png"
+  },
+  {
+    "revision": "d5d38511ac806c0342e62d6a8368e94c",
+    "url": "/react-test-calendar/static/media/Calendar.d5d38511.svg"
+  },
+  {
+    "revision": "8f20ac86b249ae147ce1f7a7b88650e6",
+    "url": "/react-test-calendar/static/media/Chat.8f20ac86.svg"
+  },
+  {
+    "revision": "1ab99b28c75702c40a42f28f6d27b99a",
+    "url": "/react-test-calendar/static/media/Close.1ab99b28.svg"
+  },
+  {
+    "revision": "81976d72e828a022ed300f89623133a8",
+    "url": "/react-test-calendar/static/media/Customers.81976d72.svg"
+  },
+  {
+    "revision": "2f16bca11ec52aeb03bc2a9c6d05d4bf",
+    "url": "/react-test-calendar/static/media/Dashboard.2f16bca1.svg"
+  },
+  {
+    "revision": "9d698f6244c79d299909cd985f518c87",
+    "url": "/react-test-calendar/static/media/Help.9d698f62.svg"
+  },
+  {
+    "revision": "9e083b2e3a5ea6bfccf5c7cbc2ef6cd8",
+    "url": "/react-test-calendar/static/media/Home.9e083b2e.svg"
+  },
+  {
+    "revision": "1255e31eb96716eea776dd79c94a7fb2",
+    "url": "/react-test-calendar/static/media/Inbox.1255e31e.svg"
+  },
+  {
+    "revision": "353a2ee34caed38847d25a118a2c173f",
+    "url": "/react-test-calendar/static/media/Invoices.353a2ee3.svg"
+  },
+  {
+    "revision": "00f12fd98cabfd7af9e70c32b44ebd1d",
+    "url": "/react-test-calendar/static/media/Logo.00f12fd9.svg"
+  },
+  {
+    "revision": "870eb32f37b96b3a48a52c05610ecea7",
+    "url": "/react-test-calendar/static/media/Notification.870eb32f.svg"
+  },
+  {
+    "revision": "91da60531102e1686f6016e61a973a03",
+    "url": "/react-test-calendar/static/media/Products.91da6053.svg"
+  },
+  {
+    "revision": "9b15ce11ed6ffff1f5c13e69468566f1",
+    "url": "/react-test-calendar/static/media/Search.9b15ce11.svg"
+  },
+  {
+    "revision": "732d77b8e125a43a0b8672c17b54fee7",
+    "url": "/react-test-calendar/static/media/Settings.732d77b8.svg"
+  },
+  {
+    "revision": "60d936525f5fb514700a2413d80197eb",
+    "url": "/react-test-calendar/static/media/Timer.60d93652.svg"
+  }
+]);
